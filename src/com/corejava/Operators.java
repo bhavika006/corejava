fbf9cd82a013ac87b574f9e3ee88f2866a375531
bhavika <bhavika006@gmail.com>
2022-08-09 18:54:30 +0530

    To learn Operators

---
+++ src/com/corejava/Operators.java
@@ -0,0 +1,47 @@
+package com.corejava;
+
+public class Operators{
+	public static void main(String[] args) {
+	    int x = 100 + 50;
+	    System.out.println(x);
+	    
+	    // This comment for Sum
+	    int sum1 = 100 + 50; 
+	    int sum2 = sum1 + 250;
+	    int sum3 = sum2 + sum2;
+	    System.out.println(sum1);
+	    System.out.println(sum2);
+	    System.out.println(sum3);  
+	    
+	    // This comment for Subtraction
+	    int a = 5;
+	    int b = 3;
+	    System.out.println(a - b);
+	    
+	    //This comment for 	Multiplication
+	    int z = 10;
+	    int y = 5;	
+	    System.out.println(z * y);
+	    
+	    // This comment for Division
+	    int p = 12;
+	    int r = 2;
+	    System.out.println(p / r);
+	    
+	    // This comment for Modulus
+	    int d = 10;
+	    int e = 6;
+	    System.out.println(d % e);
+	    
+	    // This is comment for Increment
+	    int q = 5;
+	    ++q;
+	    System.out.println(q);
+	    
+	    // This is comment for decrement
+	    int t = 10;
+	    --t;
+	    System.out.println(t);
+	    		
+	}
+}
