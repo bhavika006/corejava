2957e750c0f161567a8b4b9f49d5ed7ffec716c0
bhavika <bhavika006@gmail.com>
2022-08-09 14:58:09 +0530

    Add new Thing

--- src/com/corejava/HelloWorld.java
+++ src/com/corejava/HelloWorld.java
@@ -3,9 +3,15 @@ package com.corejava;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); // End of the comment
 		
-
-	}
-
+		// Start of the line comment
+		   System.out.println("Hello World");
+		   
+		   /*
+		     multi-line of the comment
+		    */
+		   System.out.println("Hello World");
+		   } 
+	
 }
