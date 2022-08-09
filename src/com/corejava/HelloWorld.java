68fd2af9ddfbc68b7a1750c8784d200473534d1f
bhavika <bhavika006@gmail.com>
2022-08-09 15:10:23 +0530

    Milti-line comment

--- src/com/corejava/HelloWorld.java
+++ src/com/corejava/HelloWorld.java
@@ -3,15 +3,17 @@ package com.corejava;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World"); // End of the comment
+		   System.out.println("Hello World"); // End of the comment
 		
-		// Start of the line comment
+		   // Start of the line comment
 		   System.out.println("Hello World");
 		   
 		   /*
-		     multi-line of the comment
-		    */
+		    Multi-line of the comment.
+		    This code print of the Hello World,It's Amazing.
+		   */
 		   System.out.println("Hello World");
-		   } 
+		   
+	} 
 	
 }
