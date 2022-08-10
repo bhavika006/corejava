0b9adc205504d9c4a6e0120f10e97607391f4f4a
bhavika <bhavika006@gmail.com>
2022-08-10 19:18:08 +0530

    Month demo complited

---
+++ src/com/corejava/CurrentMonthDemo.java
@@ -0,0 +1,49 @@
+package com.corejava;
+
+public class CurrentMonthDemo {
+	public static void main(String[] args) {
+		int Month = 6;
+		switch (Month) {
+		case 1 :
+		System.out.println("first month");
+		break;
+		case 2 :
+		System.out.println("second month");
+		break;
+		case 3 :
+		System.out.println("Third month");
+		break;
+		case 4 :
+		System.out.println("four month");
+		break;
+		case 5 :
+		System.out.println("Five month");
+		break;
+		case 6 :
+		System.out.println("six month");
+		break;	
+		case 7 :
+		System.out.println("Seven month");
+		break;	
+		case 8 :
+		System.out.println("Eight month");
+		break;		
+		case 9 :
+		System.out.println("Nine month");
+		break;
+		case 10:
+		System.out.println("ten month");
+		break;
+		case 11 :
+	    System.out.println("eleven month");
+	    break;
+		case 12:
+		System.out.println("twelve  month");
+		break;
+		}
+		
+		
+		
+	}
+
+}
